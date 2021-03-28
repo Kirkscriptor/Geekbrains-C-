427a39f6ccebc1bc07557ccf6766204a65259bdf
Kira <kirkscriptor@gmail.com>
2021-03-28 23:01:06 +0300

    Lesson 1 HW

---
+++ Lesson 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_1_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo en = new System.Globalization.CultureInfo("en-EN");
+            System.Threading.Thread.CurrentThread.CurrentCulture = en;
+            Console.WriteLine("Greetings, Stranger! What's your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {name!}. Today is {DateTime.Now.ToString("D")}");
+            Console.ReadKey();
+        }
+    }
+}
