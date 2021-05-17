7243297ac828561b94b14cba36d069b8e3e107a0
Kira <kirkscriptor@gmail.com>
2021-05-17 05:52:37 +0300

    Домашнее задание для 5 урока

---
+++ Lesson 5/Lesson5_Task_1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson5Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Начало задания 1
+            Console.WriteLine("Введите данные");
+            string filename = "text.txt";
+            
+            string textinput = Console.ReadLine();
+            File.AppendAllLines(filename, new[] { textinput });
+            // Конец задания 1
+        }
+    }
+}
---
+++ Lesson 5/Lesson5_Task_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson5Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Начало задания 2
+            string startup = "startup.txt";
+            File.AppendAllLines(startup, new[] { DateTime.Now.ToLongTimeString() });
+            // Конец задания 2
+
+
+        }
+    }
+}
